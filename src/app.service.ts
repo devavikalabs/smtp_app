f4f76533bfb86304fafaff3928766938245836c6
Bhagwansahay <125253093+ShyamBhagwan@users.noreply.github.com>
2025-09-02 18:20:22 +0530

    feat: integrate email functionality with NestJS Mailer module

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
-
+import { ISendMailOptions, MailerService } from '@nestjs-modules/mailer';
 @Injectable()
 export class AppService {
+  constructor(private readonly mailerService: MailerService) { }
+
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+
+  async sendMail(dto: ISendMailOptions) {
+    return this.mailerService.sendMail(dto);
   }
 }
